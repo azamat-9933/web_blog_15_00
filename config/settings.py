b5fdd7347026fc3a6f362aed58d5f9e08362c766
azamat-9933 <azamat.yakhyayeff@gmail.com>
2024-02-13 16:23:56 +0500

    last commit

--- config/settings.py
+++ config/settings.py
@@ -73,12 +73,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config['DB_NAME'],
-        'HOST': config['DB_HOST'],
-        'PORT': config['DB_PORT'],
-        'USER': config['DB_USER'],
-        'PASSWORD': config['DB_PASSWORD']
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': config['DB_NAME']
     }
 }
 
@@ -118,6 +114,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
